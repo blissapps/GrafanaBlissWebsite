29ae2d5f4d96d15caf243abf836d0067161bb481
Laerte Mello <lmello@globalshares.com>
2021-12-07 13:24:57 +0000

    Adding support for personalized logs, so we can track erros more quickly while debugging locally or in the dashboard

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 // ***********************************************************
 // This example support/index.js is processed and
 // loaded automatically before your test files.
@@ -12,9 +13,9 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
+
 // References for autocomplete
 /// <reference types="cypress"/>
-// @ts-nocheck
 
 import './commands.js'
 import 'cypress-mochawesome-reporter/register'
@@ -22,6 +23,8 @@ import 'cypress-mochawesome-reporter/register'
 // Hooks
 beforeEach(() => {
   cy.log('Cypress test: ' + Cypress.currentTest.title)
+  window.logCalls = 1
+  window.testFlow = []
 })
 
 // Import commands.js using ES2015 syntax:
@@ -32,6 +35,24 @@ import 'cypress-plugin-snapshots/commands'
 require('cypress-xpath')
 require('@cypress/skip-test/support')
 
+// Personalized logs
+Cypress.Commands.overwrite('log', (originalFn, message) => {
+  Cypress.log({
+    displayName: `--- ${window.logCalls}. ${message} ---`,
+    name: `--- ${window.logCalls}. ${message} ---`,
+    message: ''
+  })
+
+  window.testFlow.push(`${window.logCalls}. ${message}`)
+  window.logCalls++
+})
+
+// In case a test fails, always display all the logs as a workflow in the end (In case logs are being used)
+Cypress.on('fail', error => {
+  throw new Error(error + '\n\nTest flow:\n' + window.testFlow.join('\n'))
+})
+
+// General Settings for screenshots naming
 Cypress.on('test:after:run', (test, runnable) => {
   if (test.state === 'failed') {
     const screenshotFileName = `${runnable.parent.title} -- ${test.title} (failed).png`
@@ -39,7 +60,8 @@ Cypress.on('test:after:run', (test, runnable) => {
   }
 })
 
+// Returning false here prevents Cypress from failing the test
 // eslint-disable-next-line no-unused-vars
 Cypress.on('uncaught:exception', (err, runnable) => {
-  return false // returning false here prevents Cypress from failing the test
+  return false
 })
