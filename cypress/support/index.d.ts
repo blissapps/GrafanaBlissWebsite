e6f51d06218f7247ef80eb575bef69d4b529ac78
Laerte Mello <lmello@globalshares.com>
2022-02-14 11:54:40 +0000

    Including insights/tools from Cypress RealWord example app - Part 1

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -5,7 +5,7 @@
  * THEREFORE, FOR EACH COMMAND CREATED IN cypress/support/commands.js, we need to add this interface in here with the same method name and parameters.
  */
 
-// Ignore any warning error in namespace (It is expected to happen due to the TS compiler we are using in this project)
+// Ignore any warning error in namespace in case it happens (It is expected to happen due to the TS compiler we are using in this project)
 declare namespace Cypress {
   interface Chainable {
     loginSuccessfulXHRWaits(): Chainable<Element>
