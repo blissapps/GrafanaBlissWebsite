7703a8ba6850c98463e66c92b15b910472c21a28
Laerte Mello <lmello@globalshares.com>
2022-04-11 16:55:50 +0100

    Removing home.spec.js file since all tests are moved already to the official folder

--- cypress/integration/home.spec.js
+++
@@ -1,9 +0,0 @@
-import EquityAdmin from '../support/pages/equityAdmin'
-
-const equityAdmin = new EquityAdmin()
-
-describe('Home page tests', () => {
-  beforeEach(() => {
-    equityAdmin.loginPage.login()
-  })
-})
