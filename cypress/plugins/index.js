74fd3ae4334813c75591477b1f891932bf54c5e9
Laerte Mello <lmello@globalshares.com>
2022-05-30 10:37:00 +0100

    Adding routine to avoid REG MASTER enviroments to run. So, the execution will fail right in the beginning if a forbidden enviroment is placed to run

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -42,6 +42,35 @@ module.exports = (on, config) => {
 
   on('before:run', async (details) => {
     console.log('override before:run')
+
+    // Amount of specs and browser highlighted
+    if (details.specs && details.browser) {
+      // details.specs and details.browser will be undefined in interactive mode
+      console.log('\x1b[32mRunning', details.specs.length, 'specs in', details.browser.name + '\x1b[39m')
+    }
+
+    // List of forbidden environments to run automation
+    const regMasterEnvironmentsToAvoid = ['alpha-23', 'alpha23', '23']
+
+    // This routine avoids accidents, so the tests will never be allowed to run in the environments placed in the regMasterEnvironmentsToAvoid variable
+    if (details.config.env.TARGET_ENV == '' || details.config.env.TARGET_ENV == null || details.config.env.TARGET_ENV == undefined) {
+      throw new Error('\x1b[31m !!! ************** The TARGET_ENV variable MUST BE PROVIDED in the cypress.env.json file **************************** !!! \x1b[39m')
+    } else if (regMasterEnvironmentsToAvoid.includes(details.config.env.TARGET_ENV)) {
+      throw new Error(
+        '\x1b[31m !!! ************** The given TARGET_ENV"' + details.config.env.TARGET_ENV + '" IS FORBIDDEN for AT testing. Test ended. **************************** !!! \x1b[39m'
+      )
+    } else {
+      regMasterEnvironmentsToAvoid.forEach((environment) => {
+        if (details.config.baseUrl.includes(environment)) {
+          throw new Error(
+            '\x1b[31m !!! ************** The baseUrl "' + details.config.baseUrl + '" IS FORBIDDEN for AT testing. Test ended. **************************** !!! \x1b[39m'
+          )
+        }
+      })
+    }
+
+    console.log('\x1b[32mThe given environment "' + details.config.baseUrl + '" is approved for testing. Moving on...\x1b[39m')
+
     await beforeRunHook(details)
   })
 
