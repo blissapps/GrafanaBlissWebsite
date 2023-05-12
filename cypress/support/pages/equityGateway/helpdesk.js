f8222f5db4c7a421afa8a8110fb2aeebf4e5f5e8
FlavioOliveira <foliveira@globalshares.com>
2023-05-12 17:27:51 +0100

    Boiler Plate Gateway **UPDATED**
    Added helpdesk selectors, anc created the fillForm function

--- cypress/support/pages/equityGateway/helpdesk.js
+++ cypress/support/pages/equityGateway/helpdesk.js
@@ -1,10 +1,10 @@
 import BasePage from '../basePage'
-import HomePage from '../equityAdmin/homePage'
-
-const homePage = new HomePage()
 
 const selectors = {
-    //TODO
+    inputSubject: 'input[placeholder="Enter subject"]',
+    inputDescription: 'input[placeholder="Enter detailed description"]',
+    inputName: 'input[placeholder="Enter first and last name"]',
+    inputEmail: 'input[placeholder="Enter email address"]'
 }
 
 class Helpdesk extends BasePage {
@@ -12,6 +12,13 @@ class Helpdesk extends BasePage {
         this.checkUrl(Cypress.env('EQUITY_GATEWAY_BASE_URL')+'/help')
     }
 
-    //TODO
+    fillForm(sub, desc, name, email){
+        cy.get(selectors.inputSubject).type(sub)
+        cy.get(selectors.inputDescription).type(desc)
+        cy.get(selectors.inputName).type(name)
+        cy.get(selectors.inputEmail).type(email)
+
+        cy.contains('Submit').click()
+    }
 }
 export default Helpdesk
