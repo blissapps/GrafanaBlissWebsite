7b5dfb89349985771347de0d45102c813ca8c521
Paulo Moutinho <pmoutinho@globalshares.com>
2023-06-06 17:13:04 +0100

    Fixed issues with helpForm and finished pageNavi Tests

--- cypress/integration/gateway/pageNavig/pageNavi.spec.js
+++ cypress/integration/gateway/pageNavig/pageNavi.spec.js
@@ -2,18 +2,27 @@ import EquityGateway from '../../../support/pages/equityGateway'
 
 const equityGateway = new EquityGateway()
 describe('Validate Page Navi Elements', () => {
+  context('Validate User Information', () => {
+    /** Related to User Stories
+     * EGVFOUR-252
+     **/
+    it('C30159577 - Authenticating with a different user loads its respective data', () => {
+      //Test Body
+    })
+  })
+
   context('Validate Helpdesk Navigation', () => {
     /** Related to User Stories
-     * EGVFOUR-39, EGVFOUR-151
+     * EGVFOUR-***
      **/
-    it('Need to include in Testrail - Helpdesk navigation from welcome page', () => {
+    it('C******** - Helpdesk navigation from welcome page', () => {
       equityGateway.PageNavi.navigateToHelpDeskNoAuth()
     })
 
     /** Related to User Stories
-     * EGVFOUR-40
+     * EGVFOUR-***
      */
-    it('Need to include in Testrail - Helpdesk navigation from dashboard', () => {
+    it('C******** - Helpdesk navigation from dashboard', () => {
       equityGateway.LoginPage.login() //Workaround for now
       equityGateway.PageNavi.navigateToHelpDeskAuth()
     })
@@ -43,36 +52,67 @@ describe('Validate Page Navi Elements', () => {
     /** Related to User Stories
      * EGVFOUR-266
      */
-    it('C30159578 - Sales Wizard Default', () => {
+    it('C30159580 - Sales Wizard Default', () => {
       equityGateway.PageNavi.navigateToSalesWizard()
     })
 
-    it('C30159578 - Sales Wizard Security', () => {
+    /** Related to User Stories
+     * EGVFOUR-264
+     */
+    it('C30159581 - Sales Wizard Security', () => {
       equityGateway.PageNavi.navigateToSalesWizard('security')
     })
 
-    it('C30159578 - Sales Wizard Share Group', () => {
+    /** Related to User Stories
+     * EGVFOUR-264
+     */
+    it('C30159582 - Sales Wizard Share Group', () => {
       equityGateway.PageNavi.navigateToSalesWizard('share-group')
     })
 
-    it('C30159578 - Sales Wizard Amount to Sell', () => {
+    /** Related to User Stories
+     * EGVFOUR-264
+     */
+    it('C30159583 - Sales Wizard Amount to Sell', () => {
       equityGateway.PageNavi.navigateToSalesWizard('amount-to-sell')
     })
 
-    it('C30159578 - Sales Wizard Order Type', () => {
+    /** Related to User Stories
+     * EGVFOUR-264
+     */
+    it('C30159584 - Sales Wizard Order Type', () => {
       equityGateway.PageNavi.navigateToSalesWizard('order-type')
     })
 
-    it('C30159578 - Sales Wizard Distribution', () => {
+    /** Related to User Stories
+     * EGVFOUR-264
+     */
+    it('C30159585 - Sales Wizard Distribution', () => {
       equityGateway.PageNavi.navigateToSalesWizard('distribution')
     })
 
-    it('C30159578 - Sales Wizard Review Order', () => {
+    /** Related to User Stories
+     * EGVFOUR-264
+     */
+    it('C30159586 - Sales Wizard Review Order', () => {
       equityGateway.PageNavi.navigateToSalesWizard('review-order')
     })
 
-    it('C30159578 - Sales Wizard Success Confirmation', () => {
+    /** Related to User Stories
+     * EGVFOUR-264
+     */
+    it('C30159587 - Sales Wizard Success Confirmation', () => {
       equityGateway.PageNavi.navigateToSalesWizard('success-confirmation')
+    })
+  })
+
+  context('Validate All Activity', () => {
+    /** Related to User Stories
+     * EGVFOUR-266
+     */
+    it('C30159588 - My Account path navigation', () => {
+      equityGateway.LoginPage.login()
+      equityGateway.PageNavi.navigateToActivity()
     })
   })
 })
--- cypress/support/pages/equityGateway/pageNavi/pageNavi.js
+++ cypress/support/pages/equityGateway/pageNavi/pageNavi.js
@@ -3,59 +3,61 @@ import HelpForm from '../helpForm/helpForm'
 import SideMenuBar from '../sideMenuBar/sideMenuBar'
 
 // @ts-ignore
+//TODO [Fix the selectors with navigation and div dependencies]
 const selectors = {
-    welcomePageHelp: 'a.ng-star-inserted',
-    dashboardDropdown: 'gs-button.icon.medium.square.flat',
-    dropdownPersonalInfo: '.eg-header__dropdown > :nth-child(1)'
+  welcomePageHelp: 'a.ng-star-inserted',
+  dashboardDropdown: 'gs-button.icon.medium.square.flat',
+  dropdownPersonalInfo: '.eg-header__dropdown > :nth-child(1)' //FIXME (needs to be fixed nav dependent)
 }
 
 class PageNavi extends BasePage {
-    helpForm = new HelpForm()
+  helpForm = new HelpForm()
 
-    navigateToHelpDeskNoAuth(){
-        cy.visit(`${Cypress.env('EQUITY_GATEWAY_BASE_URL')}`, { failOnStatusCode: false })
-        cy.get(selectors.welcomePageHelp).click()
-        this.helpForm.validateFormElements(false)
-    }
+  /** Navigates to the Helpdesk Page without authentication validates the URL
+   */
+  navigateToHelpDeskNoAuth() {
+    cy.visit(`${Cypress.env('EQUITY_GATEWAY_BASE_URL')}`, { failOnStatusCode: false })
+    cy.get(selectors.welcomePageHelp).click()
+    this.helpForm.validateFormElements(false)
+  }
 
-    navigateToHelpDeskAuth(){
-        const sideMenuBar = new SideMenuBar()
-        sideMenuBar.support('/help')
-        this.helpForm.validateFormElements()
-    }
+  /** Navigates to Helpdesk Page through the Dashboard after authentication validates the URL
+   */
+  navigateToHelpDeskAuth() {
+    const sideMenuBar = new SideMenuBar()
+    sideMenuBar.support('/help')
+    this.helpForm.validateFormElements()
+  }
 
-    navigateToDashboard(){
-        // User logs in
-        cy.url().should('contain', '/dashboard')
-    }
+  /** After login the user is in dashboard and validates the URL
+   */
+  navigateToDashboard() {
+    // User logs in
+    cy.url().should('contain', '/dashboard')
+  }
 
-    navigateToMyAccountProfile(){
-        // User logs in and is in dashboard
-        cy.get(selectors.dashboardDropdown).click()
-        cy.get(selectors.dropdownPersonalInfo).click()
-        cy.url().should('contains', 'my-account/profile/personal-information')
-    }
+  /** Navigates to the Personal Information page and validates he URL
+   */
+  navigateToMyAccountProfile() {
+    // User logs in and is in dashboard
+    cy.get(selectors.dashboardDropdown).click()
+    cy.get(selectors.dropdownPersonalInfo).click()
+    cy.url().should('contains', 'my-account/profile/personal-information')
+  }
 
-    navigateToSalesWizard(salesWizardStep = 'default'){
-        switch (salesWizardStep){
-            case 'default': cy.visit('https://eg-v4-alpha-25.gsapps.dev/sale-wizard', { failOnStatusCode: false });
-            break;
-            case 'security': cy.visit('https://eg-v4-alpha-25.gsapps.dev/sale-wizard/security', { failOnStatusCode: false });
-            break;
-            case 'share-group': cy.visit('https://eg-v4-alpha-25.gsapps.dev/sale-wizard/share-group', { failOnStatusCode: false });
-            break;
-            case 'amount-to-sell': cy.visit('https://eg-v4-alpha-25.gsapps.dev/sale-wizard/amount-to-sell', { failOnStatusCode: false });
-            break;
-            case 'order-type': cy.visit('https://eg-v4-alpha-25.gsapps.dev/sale-wizard/order-type', { failOnStatusCode: false });
-            break;
-            case 'distribution': cy.visit('https://eg-v4-alpha-25.gsapps.dev/sale-wizard/distribution', { failOnStatusCode: false });
-            break;
-            case 'review-order': cy.visit('https://eg-v4-alpha-25.gsapps.dev/sale-wizard/review-order', { failOnStatusCode: false });
-            break;
-            case 'success-confirmation': cy.visit('https://eg-v4-alpha-25.gsapps.dev/sale-wizard/success-confirmation', { failOnStatusCode: false });
-            break;
-        }
-        cy.url().should('contain', '/sale-wizard/overview')
-    }
+  /** Navigates to the salesWizardStep page and validates if the URL is redirected to sale-wizard/overview
+   * @param {string} salesWizardStep (the desired Sales Wizard Step to navigate to)
+   */
+  navigateToSalesWizard(salesWizardStep = '') {
+    cy.visit(`https://eg-v4-alpha-25.gsapps.dev/sale-wizard/${salesWizardStep}`, { failOnStatusCode: false })
+    cy.url().should('contain', '/sale-wizard/overview')
+  }
+
+  /** Navigates to the Activity page and validates he URL
+   */
+  navigateToActivity(){
+    // User logs in
+    cy.visit('https://eg-v4-alpha-25.gsapps.dev/sale-wizard/activity', { failOnStatusCode: false })
+    cy.url().should('contain', '/activity')
+  }
 }
-export default PageNavi
