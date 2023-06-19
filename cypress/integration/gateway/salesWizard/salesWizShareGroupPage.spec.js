8e68af2c93c2b4755dcd944eb62412c4673e7e84
Paulo Moutinho <pmoutinho@globalshares.com>
2023-06-19 12:12:04 +0100

    Finished Share Group Test cases

--- cypress/integration/gateway/salesWizard/salesWizShareGroupPage.spec.js
+++ cypress/integration/gateway/salesWizard/salesWizShareGroupPage.spec.js
@@ -6,7 +6,7 @@ describe('Page Navigation Tests', () => {
         /** Related to User Stories
          * EGVFOUR-141, EGVFOUR-253
          **/
-        beforeEach(() => {
+        beforeEach('User Chooses a Security', () => {
             //NOT NECESSARY YET  - equityGateway.LoginPage.login()
             equityGateway.SalesWizBase.gotoSalesWiz()
             equityGateway.SalesWizTopBar.nextBtn('click')
@@ -14,8 +14,15 @@ describe('Page Navigation Tests', () => {
             equityGateway.SalesWizTopBar.nextBtn('click')
         })
 
-        it('Test 1', () => {
-            //equityGateway.SalesWizBase.gotoSalesWiz()
+        it('Validate Page Elements', () => {
+            //Top Bar
+            //Main Body
+            equityGateway.SalesWizShareGroupPage.validateMainBody()
+            //Share Group Border
+            //Footer
+            equityGateway.FooterBar.checkPage('Help', '/help')
+            equityGateway.FooterBar.checkPage('Privacy Policy', '/privacy-policy')
+            equityGateway.FooterBar.checkPage('Terms and Conditions', '/terms-and-conditions')
         })
     })
 })
--- cypress/support/pages/equityGateway/salesWizard/salesWizShareGroupPage.js
+++ cypress/support/pages/equityGateway/salesWizard/salesWizShareGroupPage.js
@@ -1,14 +1,32 @@
 import BasePage from '../../basePage'
 
 const selectors = {
-    nextBtn: 'gs-button[size="large"]',
-    closeBtn: ''
+  nextBtn: 'gs-button[size="large"]',
+  closeBtn: '',
+  shareGroupBody: 'gs-radio-button-group',
+  shareGroupsAvailable: 'gs-radio-button-option'
 }
 
+//#0065ff rgb(0, 101, 255) color HEX [.border-color-accent (border-color)]
+
 class salesWizShareGroupPage extends BasePage {
-    methodsHere(){
-        //FIXME Example
-        cy.get(selectors.nextBtn).contains('Next')
-    }
+  validateMainBody() {
+    this.isShareGroupOptionsBodyVisible()
+    this.areAnyShareGroupOptionsVisible()
+    this.selectShareGroupByName('Purchase plan issuances')
+    //cy.get(selectors.shareGroupsAvailable).eq(0).click().should('have.attr', 'border-color', 'rgb(0, 101, 255)')
+  }
+
+  isShareGroupOptionsBodyVisible() {
+    cy.get(selectors.shareGroupBody).should('be.visible')
+  }
+
+  areAnyShareGroupOptionsVisible() {
+    cy.get(selectors.shareGroupsAvailable).its('length').should('be.greaterThan', 0)
+  }
+
+  selectShareGroupByName(shareGroupName){
+    cy.get(selectors.shareGroupBody).contains(shareGroupName).click()
+  }
 }
 export default salesWizShareGroupPage
