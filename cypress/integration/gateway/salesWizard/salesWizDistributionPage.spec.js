a9a3f683bb3ca233d6fd8d6587135fbb23e61f36
Paulo Moutinho <pmoutinho@globalshares.com>
2023-06-26 14:52:08 +0000

    Merged in SWDistribution (pull request #22)
    SWDistribution
    
    * Finished Order Type Main Functionalities Verifications Tests
    
    * Merge branch 'blissEGDevelop' of https://bitbucket.org/globalshares/gscypressautomationframework into SWOrderType
    
    * Fixed Typo in swOrderType Spec
    
    * Finished SW Distribution tests
    
    * Merge branch 'blissEGDevelop' of https://bitbucket.org/globalshares/gscypressautomationframework into SWDistribution
    
    * Merge branch 'blissEGDevelop' of https://bitbucket.org/globalshares/gscypressautomationframework into SWDistribution
    
    * Deleted goToDistribution from salesWizDistributionPage.js
    
    
    Approved-by: Flavio Oliveira

--- cypress/integration/gateway/salesWizard/salesWizDistributionPage.spec.js
+++ cypress/integration/gateway/salesWizard/salesWizDistributionPage.spec.js
@@ -2,17 +2,35 @@ import EquityGateway from '../../../support/pages/equityGateway'
 
 const equityGateway = new EquityGateway()
 describe('Sales Wizard Distribution Page Tests', () => {
-    context('Validate User Information', () => {
-        /** Related to User Stories
-         * EGVFOUR-144
-         **/
-        beforeEach(() => {
-            //NOT NECESSARY YET  - equityGateway.LoginPage.login()
-            equityGateway.SalesWizBase.goToDistribution()
-        })
-
-        it('Test 1', () => {
-            //equityGateway.SalesWizBase.gotoSalesWiz()
-        })
+  context('Validate User Information', () => {
+    /** Related to User Stories
+     * EGVFOUR-144
+     **/
+    beforeEach(() => {
+      equityGateway.SalesWizBase.goToDistribution()
     })
+
+    it('Validate Page Elements', () => {
+      equityGateway.SalesWizDistributionPage.validatePageElements()
+    })
+
+    it('C30639346 - User selects Payroll Method', () => {
+      equityGateway.SalesWizDistributionPage.validatePayrollElements()
+    })
+
+    it('C30639347 - User selects Wire Method', () => {
+      equityGateway.SalesWizDistributionPage.validateWireElements(false)
+    })
+    it('C30639347 - User selects Urgent Wire Method', () => {
+      equityGateway.SalesWizDistributionPage.validateWireElements(true)
+    })
+
+    it('C30639347 - User selects Residential Address Method', () => {
+      equityGateway.SalesWizDistributionPage.validateAddressElements('Residential')
+    })
+
+    it('C30639347 - User selects Alternate Address Method', () => {
+      equityGateway.SalesWizDistributionPage.validateAddressElements('Alternate')
+    })
+  })
 })
